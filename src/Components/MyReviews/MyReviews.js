372f98ee5674253a51174f4d91af7c4fbcaa8417
@maruf45 <armaruf450@gmail.com>
2022-11-10 21:32:47 +0600

    update no revie

--- src/Components/MyReviews/MyReviews.js
+++ src/Components/MyReviews/MyReviews.js
@@ -15,7 +15,7 @@ const MyReviews = () => {
     })
       .then((res) => {
         if (res.status === 401 || res.status === 403) {
-         return singOut();
+          return singOut();
         }
         return res.json();
       })
@@ -41,69 +41,79 @@ const MyReviews = () => {
       <Helmet>
         <title>My service - chopwell</title>
       </Helmet>
-      <div className="container mx-auto w-[80vh] block bg-white  rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <div className="flex justify-between items-center mb-4">
-          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
-            Latest Review
-          </h5>
+      {users.length === 0 ? (
+        <div className="container mx-auto w-[80vh] block bg-white  rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex justify-between items-center mb-4">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              No review were added
+            </h5>
+          </div>
         </div>
-        <div className="flow-root">
-          <ul
-            role="list"
-            className="divide-y divide-gray-200 dark:divide-gray-700"
-          >
-            {users.map((u) => {
-              console.log(u?.email);
-              return u?.email ? (
-                <li className="pt-3 pb-0 sm:pt-4" key={u._id}>
-                  <div className="flex items-center justify-between space-x-4">
-                    <div className="flex items-center  space-x-4">
-                      <div className="flex-shrink-0">
-                        <img
-                          className="w-[100px] h-[100px] object-cover m-5 rounded-full"
-                          src={u.img}
-                        />
+      ) : (
+        <div className="container mx-auto w-[80vh] block bg-white  rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex justify-between items-center mb-4">
+            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+              Latest Review
+            </h5>
+          </div>
+          <div className="flow-root">
+            <ul
+              role="list"
+              className="divide-y divide-gray-200 dark:divide-gray-700"
+            >
+              {users.map((u) => {
+                console.log(u?.email);
+                return u?.email ? (
+                  <li className="pt-3 pb-0 sm:pt-4" key={u._id}>
+                    <div className="flex items-center justify-between space-x-4">
+                      <div className="flex items-center  space-x-4">
+                        <div className="flex-shrink-0">
+                          <img
+                            className="w-[100px] h-[100px] object-cover m-5 rounded-full"
+                            src={u.img}
+                          />
+                        </div>
+                        <div className="flex-1 min-w-0">
+                          <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                            Name: {u.fullName}
+                          </p>
+                          <p className="text-sm text-gray-500 pt-1 truncate dark:text-gray-400">
+                            Email: {u.email}
+                          </p>
+                          <p className="text-sm text-gray-500 pt-1 truncate dark:text-gray-400">
+                            Food Name: {u.food_type}
+                          </p>
+                          <p className="text-sm pt-1 pb-1.5 text-gray-500 truncate dark:text-gray-400">
+                            Review: {u.review}
+                          </p>
+                        </div>
                       </div>
-                      <div className="flex-1 min-w-0">
-                        <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
-                          Name: {u.fullName}
-                        </p>
-                        <p className="text-sm text-gray-500 pt-1 truncate dark:text-gray-400">
-                          Email: {u.email}
-                        </p>
-                        <p className="text-sm text-gray-500 pt-1 truncate dark:text-gray-400">
-                          Food Name: {u.food_type}
-                        </p>
-                        <p className="text-sm pt-1 pb-1.5 text-gray-500 truncate dark:text-gray-400">
-                          Review: {u.review}
-                        </p>
+                      <div className="flex">
+                        <Link to={`/update-review/${u._id}`} className="mr-2">
+                          <img
+                            src="https://img.icons8.com/color-glass/30/pencil.png"
+                            alt=""
+                          />
+                        </Link>
+                        <button onClick={() => handleDelete(u._id)}>
+                          <img
+                            src="https://img.icons8.com/color/30/trash.png"
+                            alt=""
+                          />
+                        </button>
                       </div>
                     </div>
-                    <div className="flex">
-                      <Link to={`/update-review/${u._id}`} className="mr-2">
-                        <img
-                          src="https://img.icons8.com/color-glass/30/pencil.png"
-                          alt=""
-                        />
-                      </Link>
-                      <button onClick={() => handleDelete(u._id)}>
-                        <img
-                          src="https://img.icons8.com/color/30/trash.png"
-                          alt=""
-                        />
-                      </button>
-                    </div>
-                  </div>
-                </li>
-              ) : (
-                <li>
-                  <p>No reviews were added</p>
-                </li>
-              );
-            })}
-          </ul>
+                  </li>
+                ) : (
+                  <li>
+                    <p>No reviews were added</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
